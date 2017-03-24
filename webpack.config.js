4c09fee1c2fb4a230db1702bcfe6a0e7c0f51500
Cristian Perrone <cristianperrone.dev@gmail.com>
2017-03-24 16:14:22 -0300

    Added HTML WebPack Plugin & some config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { resolve } = require('path')
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/app.bundle.js'
-  }
+    path: resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Test Title Plugin',
+    minify: {
+      collapseWhitespace: true
+    },
+    template: './src/tpl.index.html'
+  })]
 }
